48f64f2cf32865cf0ad17a24e9ae32f965082bb9
Xwajanuszsd <janysziwew@gmail.com>
2026-07-23 17:07:06 +0200

    ss

---
+++ android_app/MainActivity.java
@@ -0,0 +1,38 @@
+package com.danessi.bot;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+
+        // Twój serwer Railway
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://zephyr.proxy.rlwy.net:14192/dashboard/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
